b743e42efd97a95e15f4a80262901ddaff3a265e
bassem <bassembelmabrouk@live.fr>
2019-04-12 16:09:56 +0100

    change name class

--- exaTechnologiess.java
+++ exaTechnologiess.java
@@ -2,9 +2,9 @@ public class ExaTechnologies{
 	
 	public static void main (String[] args){
 	
-	system.out.println ("hello bassem");
+	System.out.println ("hello bassem");
 	
-	system.out.println ("how are you");
+	System.out.println ("how are you");
 }
 	
 }
